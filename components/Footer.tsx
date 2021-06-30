05f661dd5be24d0e11af92d4ece729abf832698b
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2021-06-30 18:38:10 -0400

    Add Disclaimer and Privacy Policy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,10 @@
+import NextLink from 'next/link';
+
 import MySocials from '@/components/MySocials';
+
+function Separator() {
+  return <span className="px-1">|</span>;
+}
 
 export default function Footer() {
   return (
@@ -6,6 +12,17 @@ export default function Footer() {
       <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-8" />
       <div className="flex justify-center max-w-2xl mx-auto w-full">
         <MySocials />
+      </div>
+      <div className="flex justify-center max-w-2xl mx-auto w-full mt-2 text-sm text-gray-400 dark:text-gray-600">
+        <span>© {new Date().getFullYear()} Nicolas Charpentier</span>
+        <Separator />
+        <NextLink href="/disclaimer">
+          <a>Disclaimer</a>
+        </NextLink>
+        <Separator />
+        <NextLink href="/privacy-policy">
+          <a>Privacy Policy</a>
+        </NextLink>
       </div>
     </footer>
   );
