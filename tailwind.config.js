9ac251de679d345b43a70536e606666ac885eb2f
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2022-07-13 09:52:31 -0400

    Add TypeScript types for Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const { spacing, fontFamily } = require('tailwindcss/defaultTheme');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
